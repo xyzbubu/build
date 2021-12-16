0b51427c8053341746d446a0da4e33b3b72790d6
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-12-17 03:58:23 +0800

    ss

--- pass/luci-app-bypass/luasrc/controller/bypass.lua
+++ pass/luci-app-bypass/luasrc/controller/bypass.lua
@@ -3,7 +3,6 @@ local fs=require"nixio.fs"
 local http=require"luci.http"
 CALL=luci.sys.call
 EXEC=luci.sys.exec
-USR=/etc/bypass
 function index()
 	if not nixio.fs.access("/etc/config/bypass") then 
 		return
@@ -32,8 +31,8 @@ end
 
 function act_status()
 	local e = {}
-	e.tcp=CALL("ps -w | grep bypass-tcp | grep -qv grep")==0
-	e.udp=CALL("ps -w | grep bypass-udp | grep -qv grep")==0
+	e.tcp = CALL('busybox ps -w | grep bypass-tcp | grep -v grep  >/dev/null ') == 0
+	e.udp = CALL('busybox ps -w | grep bypass-udp | grep -v grep  >/dev/null ') == 0
 	e.smartdns = CALL("pidof smartdns >/dev/null")==0
 
 	e.chinadns=CALL("pidof chinadns-ng >/dev/null")==0
