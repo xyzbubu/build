f5b1b234c1f0118cb8b8aae1f938bed11a5f96ca
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2022-07-14 16:49:52 +0800

    services

--- luci-app-samba4/luasrc/controller/samba4.lua
+++ luci-app-samba4/luasrc/controller/samba4.lua
@@ -7,5 +7,5 @@ function index()
 		return
 	end
 
-	entry({"admin", "control", "samba4"}, cbi("samba4"), _("Network Shares")).dependent = true
+	entry({"admin", "services", "samba4"}, cbi("samba4"), _("Network Shares")).dependent = true
 end
